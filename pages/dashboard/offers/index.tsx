8f7108b1fa739a4e3f162b681e7a1094eada1bc6
ashikmahmud <ashikmamud187@gmail.com>
2023-02-17 20:55:08 +0600

    yah design finshied of dashboard offers managemen

--- pages/dashboard/offers/index.tsx
+++ pages/dashboard/offers/index.tsx
@@ -1,6 +1,7 @@
 import DashboardLayout from "@/components/layouts/DashboardLayout";
 import Link from "next/link";
 import React from "react";
+import { BiEditAlt, BiTrash } from "react-icons/bi";
 
 type Props = {};
 
@@ -15,6 +16,38 @@ const OffersPage = (props: Props) => {
         >
           Add Offer
         </Link>
+      </div>
+
+      <div className="overflow-x-auto mt-5">
+        <table className="table w-full text-left">
+          <thead className="p-3 bg-gray-100">
+            <tr className="p-2">
+              <th className="p-2">SL No</th>
+              <th className="p-2">Offer Title</th>
+              <th className="p-2">Offer Description</th>
+              <th className="p-2">Offer Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className=" border-b">
+              <td className="p-2">1</td>
+              <td className="p-2">NextJs Developer</td>
+              <td className="p-2">
+                I really not found where I dive on the codes.....
+              </td>
+              <td className="p-2">
+                <div className="flex items-center">
+                  <button className="text-blue-500">
+                    <BiEditAlt />
+                  </button>
+                  <button className="text-red-500">
+                    <BiTrash />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
