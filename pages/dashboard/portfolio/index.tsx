d444eed39a4333eb52d61a4d1b202195913bc6cc
ashikmahmud <ashikmamud187@gmail.com>
2023-02-17 21:38:53 +0600

    done to get portfolio table

--- pages/dashboard/portfolio/index.tsx
+++ pages/dashboard/portfolio/index.tsx
@@ -1,6 +1,7 @@
 import DashboardLayout from "@/components/layouts/DashboardLayout";
 import Link from "next/link";
 import React from "react";
+import { BiEditAlt, BiLink, BiTrash } from "react-icons/bi";
 
 type Props = {};
 
@@ -15,6 +16,98 @@ const PortfolioPage = (props: Props) => {
         >
           Add Portfolio
         </Link>
+      </div>
+      <div className="overflow-x-auto my-3 bg-white p-2">
+        <table className="table w-full text-left">
+          <thead className=" bg-gray-100">
+            <tr className="">
+              <th className="p-3 font-medium">SL No</th>
+              <th className="p-3 font-medium">Portfolio Title</th>
+              <th className="p-3 font-medium">Portfolio Category</th>
+              <th className="p-3 font-medium">Portfolio Description</th>
+              <th className="p-3 font-medium">Portfolio Links</th>
+              <th className="p-3 font-medium">Portfolio Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className=" border-b">
+              <td className="p-2">1</td>
+              <td className="p-2">
+                <div className="flex items-center gap-2">
+                  <img
+                    className="w-14 h-14 rounded object-cover border"
+                    src="https://raw.githubusercontent.com/Ashik-Mahmud/nextjs-official-portfolio/main/preview.png"
+                    alt="profile"
+                  />
+                  <div className="flex flex-col gap-1">
+                    <span className="font-bold">NextJs Developer</span>
+
+                    <div className="flex flex-wrap gap-1">
+                      <span className="bg-gray-100  p-1 border rounded  text-xs ">
+                        NextJs
+                      </span>
+                      <span className="bg-gray-100  p-1 border rounded  text-xs ">
+                        NextJs
+                      </span>
+                      <span className="bg-gray-100  p-1 border rounded  text-xs ">
+                        NextJs
+                      </span>
+                      <span className="bg-gray-100  p-1 border rounded  text-xs ">
+                        NextJs
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td className="p-2">
+                <span className="text-blue-500 bg-blue-50 p-2 px-4">
+                  Web Development
+                </span>
+              </td>
+              <td className="p-2">
+                I really not found where I dive on the codes.....
+              </td>
+              <td className="p-2">
+                <div className="flex items-center gap-2">
+                  <a
+                    className="text-blue-500 flex items-center gap-1 text-sm"
+                    href="https://ashikmahmud.me"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <BiLink /> Live
+                  </a>
+                  <a
+                    className="text-blue-500 flex items-center gap-1 text-sm"
+                    href="https://ashikmahmud.me"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <BiLink /> Client Live
+                  </a>
+                  <a
+                    className="text-blue-500 flex items-center gap-1 text-sm"
+                    href="https://ashikmahmud.me"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <BiLink /> Server Live
+                  </a>
+                </div>
+              </td>
+              <td className="p-2">
+                <div className="flex items-center">
+                  <button className="text-blue-500">
+                    <BiEditAlt />
+                  </button>
+                  <button className="text-red-500">
+                    <BiTrash />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
