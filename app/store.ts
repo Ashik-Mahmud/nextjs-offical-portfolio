472b3b6cebc286b6e526fed94cb55982e37549bd
ashikmahmud <ashikmamud187@gmail.com>
2023-02-28 19:41:48 +0600

    config first rtk api to the store which is authentication

--- app/store.ts
+++ app/store.ts
@@ -1,12 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import AuthenticationApi from "@/apis/authenticationApi";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 const store = configureStore({
   reducer: {
     // Add your reducers here
+    [AuthenticationApi.reducerPath]: AuthenticationApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(AuthenticationApi.middleware),
+
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
